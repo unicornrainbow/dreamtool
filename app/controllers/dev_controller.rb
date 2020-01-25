2cfec6c75f260b34e6a281f719ba3d4d5210911d
Bratty Princess <@>
2020-01-25 15:35:39 -0500

    autosnapped

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -104,7 +104,7 @@ class DevController < ApplicationController
 
 
     @files = k.map do |filepath|
-      [FileUtils.filename(filepath), filepath]
+      [filepath.split("/").last, filepath]
     end
     @sauce = sauce
     render :browse, layout: false
