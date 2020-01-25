ad3ebef39f2a102f6fc1480b27cc081f75eea397
Bratty Princess <@>
2020-01-25 13:37:12 -0500

    autosnapped
    working on autocommit

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -79,7 +79,9 @@ class DevController < ApplicationController
 
     # render text: `ls -R #{File.join(Rails.root, 'app/assets/javascripts')}`
     Dir.chdir(File.join(Rails.root, 'app/assets/javascripts'))
-    render text: Dir.glob('**/*').select {|f| /#{d}/.match f }
+    k = Dir.glob('**/*').select {|f| /#{d}/.match f }
+
+    render text: k.last 
        # true }
 
   end
