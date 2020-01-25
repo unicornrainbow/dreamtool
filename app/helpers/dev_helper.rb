da1bda5dfa46d2758cc37c6c518202ecf3fd5002
Bratty Princess <@>
2020-01-25 15:46:17 -0500

    autosnapped

--- app/helpers/dev_helper.rb
+++ app/helpers/dev_helper.rb
@@ -1,5 +1,22 @@
 module DevHelper
   def read_file(path)
-    File.read path
+    sauce = File.read path
+    # sauce = File.read File.join(Rails.root, 'app/assets/javascripts', k.last)
+    sauce = sauce.lines.map do |line|
+      if line =~ /(^.*)new ([\w\.]*)(.*)$/
+        s,a,v = $1,$2,$3
+        href = $2
+        href = "/dev/browse/" + href
+        # href.sub!("Newstime.","")
+        # href = href.underscore
+        # href = ""
+
+        "->" + [s,"new ",
+          "<a href=\"",href,"\">",
+          a,"</a>",v,"\n"].join
+      else
+        line
+      end
+    end.join
   end
 end
