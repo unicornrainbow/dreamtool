e697612aa5fd08bd8287f9582aff6df1a6415e48
Bratty Princess <@>
2020-01-25 13:40:13 -0500

    autosnapped
    working on autocommit

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -81,9 +81,24 @@ class DevController < ApplicationController
     Dir.chdir(File.join(Rails.root, 'app/assets/javascripts'))
     k = Dir.glob('**/*').select {|f| /#{d}/.match f }
 
-    r = File.read File.join(Rails.root, 'app/assets/javascripts', k.last)
-    render text: "<pre>#{r}</pre>"
-       # true }
+    sauce = File.read File.join(Rails.root, 'app/assets/javascripts', k.last)
+    sauce = sauce.lines.map do |line|
+      if line =~ /(^.*)new ([\w\.]*)(.*)$/
+        s,a,v = $1,$2,$3
+        href = $2
+        href = "/dev/browse/" + href
+        # href.sub!("Newstime.","")
+        # href = href.underscore
+        # href = ""
+
+        "->" + [s,"new ",
+          "<a href=\"",href,"\">",
+          a,"</a>",v,"\n"].join
+      else
+        line
+      end
+    end.join
+    render text: "<pre>#{sauce}</pre>"
 
   end
 
