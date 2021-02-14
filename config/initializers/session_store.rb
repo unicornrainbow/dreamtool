4f9c49ad10807ca1d1a67149a0eea9fcb5ba7ce4
Bambi <bambi@cookie>
2021-02-14 17:31:34 -0500

    I can't believe it's not BUTTER!

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Press::Application.config.session_store :cookie_store, key: '_press_session'
+Press::Application.config.session_store :cookie_store, key: 'xoxo', secure: Rails.env.production?
