524a79d6b59e4575f57c46246e655a4f9b3440d0
Bambi <bambi@cookie>
2021-05-02 14:26:02 -0400

    find by email user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,8 +17,16 @@ class UserTest < ActiveSupport::TestCase
     assert user.authenticate("12341234")
   end
 
-  test "User can have no password" do
-    user = User.create(screenname: "Plastic")
-    refute user.has_password?
+  test "Find user by email" do
+    # user = User.create(email: "123@xyz.net")
+
+    user = User.create(screenname: "Plastic",
+                       email: "123@xyz.net",
+                       password: "12341234")
+
+    user = User.find_by(email: "123@xyz.net")
+    assert_equal user.email, "123@xyz.net"
+
   end
+  
 end
