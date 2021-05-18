2bed107ebf18f27e9e8e27e97305c20325b4a1ba
Bambi <bambi@cookie>
2021-05-18 19:17:23 -0400

    Cupcake test reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
 
 class ActiveSupport::TestCase
   include Mongoid::FixtureSet::TestHelper
@@ -19,3 +20,27 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class TestReporter < Minitest::Reporters::DefaultReporter
+
+  def cupcake
+    "\u{1F9C1}"
+  end
+
+  def glassofmilk
+    "\u{1F95B}"
+  end
+
+  def flower;"\u{1F338}" end
+  def tulip ; "\u{1F337}" end
+
+  def green(string)
+    if string == "."
+      [cupcake, glassofmilk].sample
+    else
+      string
+    end
+  end
+
+end
+Minitest::Reporters.use! [TestReporter.new]
